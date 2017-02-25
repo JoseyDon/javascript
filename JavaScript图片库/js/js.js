2eec37a92a0631fc89f579bd1b08ba7a4997df1e
JoseyDon <joseydon@163.com>
2017-02-25 12:20:49 +0800

    js

--- JavaScript图片库/js/js.js
+++ JavaScript图片库/js/js.js
@@ -1,12 +1,33 @@
+//分离JavaScript和HTML标签
+function prepareGallery(){
+	if (!document.getElementsByTagName) {return false;}
+	if (!document.getElementById) {return false;}
+	if (!document.getElementById("ImagesGallery")) {return false;}
+	var gallery = document.getElementById("ImagesGallery");
+	var links = gallery.getElementsByTagName("a");
+	for(var i = 0;i<links.length;i++){
+		links[i].onclick = function(){
+			return !showPic(this);
+		}
+		links[i].onkeypress = links[i].onclick;
+	}
+}
 //点击链接占位图及文字更改为相应内容
 	function showPic(whichpic){
+		if (!document.getElementById('placeholder')) {return false;}
 		var placeholder = document.getElementById('placeholder');
 		var source = whichpic.getAttribute("href");
+		if (placeholder.nodeName != 'IMG') {return false;}
 		placeholder.setAttribute("src",source);
-		var text = whichpic.getAttribute("title");
+		if (document.getElementById('description')) {
+		var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
 		var description = document.getElementById('description');
+		if(description.firstChild.nodeType == 3){
 		description.firstChild.nodeValue = text;
 	}
+	}
+	return true;
+		}
 
 /*childNodes nodeTYPE的应用
 	function countBodyChildren(){
@@ -16,3 +37,42 @@
 	}
 	window.onload=countBodyChildren;
 */
+//把函数绑定到load事件。
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else{
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
+
+addLoadEvent(prepareGallery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
